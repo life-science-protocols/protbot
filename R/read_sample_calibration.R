fe5104ec50c2b737630d6d411e66c96db7b0e33a
Ramiro Magno <rmagno@pattern.institute>
2023-11-18 10:59:13 +0000

    Add `read_sample_calibration()` function

---
+++ R/read_sample_calibration.R
@@ -0,0 +1,16 @@
+#' Read a sample calibration file
+#'
+#' This function reads a calibration file that is bundled with protbot.
+#'
+#' @param file A string with the sample calibration file name.
+#'
+#' @returns A data frame resulting from the reading of the sample calibration
+#'   file pointed by `file`.
+#'
+read_sample_calibration <-
+  function(file = stop("Please provide a `file` path")) {
+    rel_path <- glue::glue("datasets/{file}")
+    abs_path <- system.file(rel_path, package = "protbot")
+
+    read.csv(abs_path)
+  }
